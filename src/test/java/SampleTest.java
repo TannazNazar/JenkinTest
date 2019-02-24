147920e603683f7546925c1eeea0639ae6833562
Tannaz <Tanaz.nazar@gmail.com>
2019-02-24 18:26:02 -0500

    New update

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SampleTest {
+	
+	
+	@Test
+	public void Test() {
+		
+		System.out.println("This is simple Test");
+		Assert.assertTrue(true);
+	}
+
+}
